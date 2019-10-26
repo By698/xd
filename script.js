8330d3dabdf6cedd9eb74138c52b1555f85fe6c9
By698 <bysiek1998@gmail.com>
2019-10-26 22:23:56 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let fadein = () => {
+    let h1 = document.querySelector("h1");
+    let article = document.querySelector("article");
+    article.classList.toggle("show");
+    h1.classList.toggle("hide")
+
+}
